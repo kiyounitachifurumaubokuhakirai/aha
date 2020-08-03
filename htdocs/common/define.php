9a2d6f7b041da0b86493a8dea33448d1c17603cb
M <sonegaukyokonogorodesu+github@gmail.com>
2020-08-03 15:02:46 +0900

    共通の文字、関数を作成

---
+++ htdocs/common/define.php
@@ -0,0 +1,27 @@
+<?php
+    /** Database接続関連 */
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'Booklist');
+    define('DB_USER', 'root');
+    //define('DB_PASS', '');   //for windows
+    define('DB_PASS', 'root');   //for mac
+
+
+    /** エラーリクエスト関連 */
+    define('MSG_ERR', '%sに誤りがあります。');
+    define('MSG_EXCEPTION', '申し訳ございません、エラーが発生しました。');
+
+    
+    /** サニタイズ
+     * @param array $before サニタイズしたいデータ
+     * 返り値   ：  サニタイズ完了したデータ
+     */
+    function sanitize($before){
+        $after=[];
+        foreach($before as $key => $value){
+        $after[$key]=htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+        }
+        return $after;
+    }
+
+?>
