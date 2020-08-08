716bf5be14d8229f4b16a9813ebeb4c7fbd169a1
M <sonegaukyokonogorodesu+github@gmail.com>
2020-08-08 16:54:07 +0900

    DB_NAMEを修正

--- htdocs/common/define.php
+++ htdocs/common/define.php
@@ -1,7 +1,7 @@
 <?php
     /** Database接続関連 */
     define('DB_HOST', 'localhost');
-    define('DB_NAME', 'Booklist');
+    define('DB_NAME', 'AHA');
     define('DB_USER', 'root');
     //define('DB_PASS', '');   //for windows
     define('DB_PASS', 'root');   //for mac
